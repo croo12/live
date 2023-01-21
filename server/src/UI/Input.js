85a842f07f7b0e93ff2b301d2436ece28a2ac5e7
its19447@gmail.com <its19447@gmail.com>
2023-01-22 00:09:05 +0900

    :lipstick: Change Component And Add CSS

--- server/src/UI/Input.js
+++ server/src/UI/Input.js
@@ -1,5 +1,20 @@
-const Input = () => {
-  return <input />;
+import classes from "./Input.module.scss";
+
+const Input = (props) => {
+  //강의 베낌
+  return (
+    <div className={`${classes.inputBox} ${props.isValid ? "none" : ""}`}>
+      <input
+        className={classes.slideInput}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+      <label htmlFor={props.id}>{props.label}</label>
+    </div>
+  );
 };
 
 export default Input;
