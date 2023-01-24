43d176b9f7ddccb153f4354b0096e635ba2597c5
김영주 <eorhrtnv@naver.com>
2023-01-24 22:05:20 +0900

    Change SignUp

--- server/src/components/SignUpPageRealtor.js
+++ server/src/components/SignUpPageRealtor.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 const SignUpPageRealtor = () => {
@@ -8,47 +8,112 @@ const SignUpPageRealtor = () => {
     console.log(e.target.phone.value);
   };
 
+  const [realtorEmail, setRealtorEmail] = useState("");
+  const [realtorPass, setRealtorPass] = useState("");
+  const [realtorPassCheck, setRealtorPassCheck] = useState("");
+
+  const [realtorEmailError, setRealtorEmailError] = useState(false);
+  const [realtorPassError, setRealtorPassError] = useState(false);
+  const [realtorPassCheckError, setRealtorPassCheckError] = useState(false);
+
+  const onChangeRealtorEmail = (e) => {
+    if (e.target.value.includes("@")) setRealtorEmailError(false);
+    else setRealtorEmailError(true);
+
+    setRealtorEmail(e.target.value);
+  };
+
+  const onChangeRealtorPass = (e) => {
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$^*+=-])(?=.*[0-9]){9,16}/;
+    if (!e.target.value || passwordRegex.test(e.target.value))
+      setRealtorPassError(false);
+    else setRealtorPassError(true);
+
+    if (!realtorPassCheck || e.target.value === realtorPassCheck) {
+      setRealtorPassCheckError(false);
+    } else setRealtorPassCheckError(true);
+
+    setRealtorPass(e.target.value);
+  };
+
+  const onChangeRealtorPassCheck = (e) => {
+    if (e.target.value === realtorPass) setRealtorPassCheckError(false);
+    else setRealtorPassCheckError(true);
+
+    setRealtorPassCheck(e.target.value);
+  };
+
   return (
     <>
       <form onSubmit={onChangeHanldler}>
         <button>조회하기</button>
         <div>
-          <label htmlFor="business_number">사업자 상호 </label>
-          <input id="business_number" name="business_number" />
+          <label htmlFor="businessNumber">사업자 상호 </label>
+          <input id="businessNumber" name="businessNumber" type="text" />
         </div>
         <div>
-          <label htmlFor="realtor_name">사업자 대표명 </label>
-          <input id="realtor_name" name="realtor_name" />
+          <label htmlFor="realtorName">사업자 대표명 </label>
+          <input id="realtorName" name="realtorName" type="text" />
         </div>
         <div>
-          <label htmlFor="Field">중개등록번호</label>
-          <input id="Field" name="Field" />
+          <label htmlFor="field">중개등록번호</label>
+          <input id="field" name="field" type="text" />
         </div>
         <div>
-          <label htmlFor="business_address">주소</label>
-          <input id="business_address" name="business_address" />
+          <label htmlFor="businessAddress">주소</label>
+          <input id="businessAddress" name="businessAddress" type="text" />
         </div>
         <div>
-          <label htmlFor="realtor_phone">전화번호</label>
-          <input id="realtor_phone" name="realtor_phone" />
+          <label htmlFor="realtorPhone">전화번호</label>
+          <input id="realtorPhone" name="realtorPhone" type="text" />
         </div>
         <div>
-          <label htmlFor="realtor_email">이메일</label>
-          <input id="realtor_email" name="realtor_email" />
+          <label htmlFor="realtorEmail">이메일</label>
+          <input
+            id="realtorEmail"
+            name="realtorEmail"
+            type="email"
+            value={realtorEmail}
+            onChange={onChangeRealtorEmail}
+          />
+          {realtorEmailError && (
+            <div style={{ color: "red" }}>올바른 이메일 형식이 아닙니다!</div>
+          )}
         </div>
         <div>
-          <label htmlFor="business_number">사업자 등록번호</label>
-          <input id="business_number" name="business_number" />
+          <label htmlFor="businessNumber">사업자 등록번호</label>
+          <input id="businessNumber" name="businessNumber" type="text" />
           <button>중복확인</button>
         </div>
         <div>
-          <label htmlFor="realtor_pass">비밀번호</label>
-          <input id="realtor_pass" name="realtor_pass" />
+          <label htmlFor="realtorPass">비밀번호</label>
+          <input
+            id="realtorPass"
+            name="realtorPass"
+            type="text"
+            value={realtorPass}
+            onChange={onChangeRealtorPass}
+          />
+          {realtorPassError && (
+            <div style={{ color: "red" }}>
+              비밀번호는 문자,숫자,특수문자를 조합하여 9자이상 16자 이내이어야
+              합니다.
+            </div>
+          )}
         </div>
         <div>
-          <label htmlFor="realtor_passcheck">비밀번호 확인</label>
-          <input id="realtor_passcheck" name="realtor_passcheck" />
+          <label htmlFor="realtorPassCheck">비밀번호 확인</label>
+          <input
+            id="realtorPassCheck"
+            name="realtorPassCheck"
+            type="text"
+            value={realtorPassCheck}
+            onChange={onChangeRealtorPassCheck}
+          />
         </div>
+        {realtorPassCheckError && (
+          <div style={{ color: "red" }}>비밀번호 입력값이 다릅니다!</div>
+        )}
         <button>중개사 가입</button>
       </form>
       <Outlet />
--- server/src/components/SignUpPageUser.js
+++ server/src/components/SignUpPageUser.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 const SignUpPageUser = () => {
@@ -8,33 +8,117 @@ const SignUpPageUser = () => {
     console.log(e.target.phone.value);
   };
 
+  const [userId, setUserId] = useState("");
+  const [userPass, setUserPass] = useState("");
+  const [userPassCheck, setUserPassCheck] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+
+  // 에러 메세지
+  const [userIdError, setUserIdError] = useState(false);
+  const [userPassError, setUserPassError] = useState(false);
+  const [userPassCheckError, setUserPassCheckError] = useState(false);
+  const [userEmailError, setUserEmailError] = useState(false);
+
+  const onChangeUserId = (e) => {
+    if (e.target.value.length > 16) setUserIdError(true);
+    else setUserIdError(false);
+
+    setUserId(e.target.value);
+  };
+
+  const onChangeUserPass = (e) => {
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$^*+=-])(?=.*[0-9]){9,16}/;
+    if (!e.target.value || passwordRegex.test(e.target.value))
+      setUserPassError(false);
+    else setUserPassError(true);
+
+    if (!userPassCheck || e.target.value === userPassCheck)
+      setUserPassCheckError(false);
+    else setUserPassCheckError(true);
+
+    setUserPass(e.target.value);
+  };
+
+  const onChangeUserPassCheck = (e) => {
+    if (userPass === e.target.value) setUserPassCheckError(false);
+    else setUserPassCheckError(true);
+
+    setUserPassCheck(e.target.value);
+  };
+
+  const onChangeEmail = (e) => {
+    if (e.target.value.includes("@")) setUserEmailError(false);
+    else setUserEmailError(true);
+
+    setUserEmail(e.target.value);
+  };
+
   return (
     <>
       <form onSubmit={onChangeHanldler}>
         <div>
           <label htmlFor="user_id">아이디 </label>
-          <input id="user_id" name="user_id" />
+          <input
+            id="userId"
+            name="userId"
+            type="text"
+            value={userId}
+            onChange={onChangeUserId}
+          />
+          {userIdError && (
+            <div style={{ color: "red" }}>최대 16자까지 입력가능합니다.</div>
+          )}
           <button>중복확인</button>
         </div>
         <div>
-          <label htmlFor="user_pass">비밀번호 </label>
-          <input id="user_pass" name="user_pass" />
+          <label htmlFor="userPass">비밀번호 </label>
+          <input
+            id="userPass"
+            name="userPass"
+            type="password"
+            value={userPass}
+            onChange={onChangeUserPass}
+          />
+          {userPassError && (
+            <div style={{ color: "red" }}>
+              비밀번호는 문자,숫자,특수문자를 조합하여 9자이상 16자 이내이어야
+              합니다.
+            </div>
+          )}
         </div>
         <div>
-          <label htmlFor="user_passcheck">비밀번호 확인 </label>
-          <input id="user_passcheck" name="user_passcheck" />
+          <label htmlFor="userPassCheck">비밀번호 확인 </label>
+          <input
+            id="userPassCheck"
+            name="userPassCheck"
+            type="password"
+            value={userPassCheck}
+            onChange={onChangeUserPassCheck}
+          />
+          {userPassCheckError && (
+            <div style={{ color: "red" }}>비밀번호 입력값이 다릅니다!</div>
+          )}
         </div>
         <div>
-          <label htmlFor="user_name">이름 </label>
-          <input id="user_name" name="user_name" />
+          <label htmlFor="userName">이름 </label>
+          <input id="userName" name="userName" type="text" />
         </div>
         <div>
-          <label htmlFor="user_email">이메일 </label>
-          <input id="user_email" name="user_email" />
+          <label htmlFor="userEmail">이메일 </label>
+          <input
+            id="userEmail"
+            name="userEmail"
+            type="email"
+            value={userEmail}
+            onChange={onChangeEmail}
+          />
+          {userEmailError && (
+            <div style={{ color: "red" }}>올바른 이메일 형식이 아닙니다!</div>
+          )}
         </div>
         <div>
-          <label htmlFor="user_phone">전화번호 </label>
-          <input id="user_phone" name="user_phone" />
+          <label htmlFor="userPhone">전화번호 </label>
+          <input id="userPhone" name="userPhone" type="text" />
         </div>
         <button>회원 가입</button>
       </form>
