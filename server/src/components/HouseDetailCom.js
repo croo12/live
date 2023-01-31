ad0423a779243142253cb35b847efeecc561d986
신현광 <rhkd0363@gmail.com>
2023-01-31 16:04:31 +0900

    :white_check_mark: Update House Detail Component
    Update House Detail Component

--- server/src/components/HouseDetailCom.js
+++ server/src/components/HouseDetailCom.js
@@ -4,6 +4,7 @@ import tempImage3 from "../assets/image/MainBackground.png";
 import tempImage4 from "../assets/image/MainPicture.png";
 import CarouselTemp from "./house/CarouselTemp";
 import Map from "../UI/Map";
+import Icons from "../assets/Icons";
 
 const HouseDetailCom = (props) => {
   // 캐러셀 설정 값
@@ -42,6 +43,16 @@ const HouseDetailCom = (props) => {
     { imgSrc: tempImage2 },
   ];
 
+  const seeMore = () => {
+    document.getElementById("moreInfoBtn").style.display = "none";
+    document.getElementById("houseInfo").style.display = "";
+  };
+
+  const foldHandler = () => {
+    document.getElementById("moreInfoBtn").style.display = "";
+    document.getElementById("houseInfo").style.display = "none";
+  };
+
   // 등록 번호 같은거 필요
   // 유저 입장 -> 매물 검색으로 접근 -> 동일 매물에 여러 중개사가 있는 경우 ->
   return (
@@ -59,7 +70,7 @@ const HouseDetailCom = (props) => {
       </div>
       <div>
         <p> 주소 -&gt; 고정</p>
-        유성구 덕명동
+        대전시 유성구 덕명동( 동까지만 ) - 상세주소는 매물 정보에
       </div>
       <div>
         <p> 월세/보증금/관리비 -&gt; 유저가 매물 검색으로 들어갔을 때만</p>
@@ -74,10 +85,52 @@ const HouseDetailCom = (props) => {
       </div>
       <div>
         <p> 매물정보 -&gt; 고정</p>
+        <p>간단 설명 : 2층 복층 풀옵션 귀한 방</p>
+        <div>{Icons.area} 전용 33.33m² / 공급 33.33m²</div>
+        <div>{Icons.room} 방 2개( 욕실 1개 ) - 원룸 투룸??</div>
+        <div>{Icons.buildingFloor} 2층/7층 (층수/전체층수)</div>
+        <div>{Icons.parking} 주차 가능/주차 불가능 (주차여부)</div>
+        <div>{Icons.calendar} 2023.2.17/즉시 입주 가능 (입주가능일)</div>
+        <button id="moreInfoBtn" onClick={seeMore}>
+          더보기
+        </button>
+        <div id="houseInfo" style={{ display: "none" }}>
+          <div>{Icons.compass} 동 서 남 북 북동 북서..(방향)</div>
+          <div>{Icons.elevator} 엘리베이터 있음/없음</div>
+          <div>{Icons.purpose} 다세대주택(건축물 용도)</div>
+          <div>{Icons.marker} 유성구 덕명동 12-34</div>
+          <button onClick={foldHandler}>접기</button>
+        </div>
         <hr />
       </div>
       <div>
         <p> 옵션들 -&gt; 유저가 매물 검색으로 들어갔을 때만</p>
+        <div>{Icons.bed} 침대</div>
+        <div>{Icons.washingMachine} 세탁기</div>
+        <div>{Icons.airConditioner} 에어컨</div>
+        <div>{Icons.desk} 책상</div>
+        <div>{Icons.closet} 옷장</div>
+        <div>{Icons.bathtub} 욕조</div>
+        <div>{Icons.sink} 싱크대</div>
+        <div>{Icons.cctv} CCTV</div>
+        <div>{Icons.table} 식탁</div>
+        <div>{Icons.sofa} 소파</div>
+        <div>{Icons.shoeRack} 신발장</div>
+        <div>{Icons.refrigerator} 냉장고</div>
+        <div>{Icons.dryingMachine} 건조기</div>
+        <div>{Icons.bidet} 비데</div>
+        <div>{Icons.dishWasher} 식기세척기</div>
+        <div>{Icons.gasStore} 가스레인지</div>
+        <div>{Icons.inductionCooktop} 인덕션</div>
+        <div>{Icons.microwave} 전자레인지</div>
+        <div>{Icons.gasOven} 오븐</div>
+        <div>{Icons.guard} 경비원</div>
+        <div>{Icons.intercom} 인터폰</div>
+        <div>{Icons.keycard} 카드키</div>
+        <div>{Icons.fireAlarm} 화재경보기</div>
+        <div>{Icons.veranda} 베란다</div>
+        <div>{Icons.terrace} 테라스</div>
+        <div>{Icons.garden} 마당</div>
         <hr />
       </div>
       <div>
