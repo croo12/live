79a4788196b2d0e2055a201fdb093a263c2762e6
김혜정 <mnb3559@gmail.com>
2023-01-31 11:08:53 +0900

    :sparkles: Add WebRTC
    Add WebRTC

--- server/Live/src/main/java/com/ssafy/live/webrtc/WebRTCConfig.java
+++ server/Live/src/main/java/com/ssafy/live/webrtc/WebRTCConfig.java
@@ -1,18 +1,17 @@
 /*
- * (C) Copyright 2014 Kurento (http://kurento.org/)
+ * Copyright 2018 Kurento (https://www.kurento.org)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.ssafy.live.webrtc;
@@ -21,34 +20,28 @@ import org.kurento.client.KurentoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 /**
- * Video call 1 to 1 demo (main).
- *
- * @author Boni Garcia (bgarcia@gsyc.es)
- * @author Micael Gallego (micael.gallego@gmail.com)
- * @since 4.3.1
+ * Kurento Java Tutorial - Application entry point.
  */
-@SpringBootApplication
+@Configuration
 @EnableWebSocket
-public class One2OneCallApp implements WebSocketConfigurer {
-
+public class WebRTCConfig implements WebSocketConfigurer
+{
   @Bean
-  public CallHandler callHandler() {
-    return new CallHandler();
+  public Handler handler()
+  {
+    return new Handler();
   }
 
   @Bean
-  public UserRegistry registry() {
-    return new UserRegistry();
-  }
-
-  @Bean
-  public KurentoClient kurentoClient() {
+  public KurentoClient kurentoClient()
+  {
     return KurentoClient.create();
   }
 
@@ -60,7 +53,9 @@ public class One2OneCallApp implements WebSocketConfigurer {
   }
 
   @Override
-  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-    registry.addHandler(callHandler(), "/call");
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry)
+  {
+    registry.addHandler(handler(), "/helloworld").setAllowedOrigins("*");
   }
+
 }
