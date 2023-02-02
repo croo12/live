56263dcf0b5d4b98b9980d6c1910a59868d12822
신현광 <rhkd0363@gmail.com>
2023-02-03 02:41:20 +0900

    :lipstick: Update Header Format and Style
    Update Header Format and Style

--- server/src/components/common/Header.js
+++ server/src/components/common/Header.js
@@ -2,57 +2,67 @@ import { NavLink } from "react-router-dom";
 import classes from "./Header.module.scss";
 import logo from "../../assets/image/liveLogo.png";
 import { makeUUID } from "../../util/UUID";
+import { useState } from "react";
+
+import { TiThMenu } from "react-icons/ti";
+
 const Header = () => {
+  const [isToggled, setIsToggled] = useState(false);
+
+  const isToggledHandler = () => {
+    setIsToggled(!isToggled);
+  };
+
   return (
     <div className={classes.header}>
-      <img src={logo} className={classes.logo} />
-      <span className={classes.nav}>
-        <NavLink to={"/"} style={{ textDecoration: "none", color: "black" }}>
-          홈
-        </NavLink>
-        <NavLink
-          to={"/login"}
-          style={{ textDecoration: "none", color: "black" }}
-        >
-          {" "}
-          로그인{" "}
-        </NavLink>
-        <NavLink
-          to={"/signup"}
-          style={{ textDecoration: "none", color: "black" }}
-        >
-          {" "}
-          회원가입{" "}
-        </NavLink>
-        <NavLink
-          to={"/reservation"}
-          style={{ textDecoration: "none", color: "black" }}
-        >
-          {" "}
-          예약{" "}
-        </NavLink>
-        <NavLink
-          to={`/consulting/${makeUUID()}`}
-          style={{ textDecoration: "none", color: "black" }}
-        >
-          {" "}
-          상담{" "}
-        </NavLink>
-        <NavLink
-          to={"/mypage"}
-          style={{ textDecoration: "none", color: "black" }}
-        >
-          {" "}
-          나{" "}
-        </NavLink>
-        <NavLink
-          to={"/house"}
-          style={{ textDecoration: "none", color: "black" }}
-        >
-          {" "}
-          매물{" "}
-        </NavLink>
-      </span>
+      <div className={classes.headerInner}>
+        <div className={classes.item}>
+          <NavLink to={"/"} onClick={isToggled && isToggledHandler}>
+            <img src={logo} alt="Live Logo" />
+          </NavLink>
+          <button className={classes.toggleBtn}>
+            <TiThMenu onClick={isToggledHandler} />
+          </button>
+        </div>
+        <ul className={`${classes["menu"]} ${isToggled && classes.active}`}>
+          <li>
+            <NavLink to={"/house"} onClick={isToggled && isToggledHandler}>
+              매물
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/reservation"}
+              onClick={isToggled && isToggledHandler}
+            >
+              예약
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={`/consulting/${makeUUID()}`}
+              onClick={isToggled && isToggledHandler}
+            >
+              상담
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/mypage"} onClick={isToggled && isToggledHandler}>
+              마이페이지
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/login"} onClick={isToggled && isToggledHandler}>
+              로그인
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/signup"} onClick={isToggled && isToggledHandler}>
+              회원가입
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
