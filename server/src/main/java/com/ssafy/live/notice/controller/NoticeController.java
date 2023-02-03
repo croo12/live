5852bad2829cd47ad77a7f9daf9ab4a4e3a7d592
BaekJeongEun <sophia2404@naver.com>
2023-02-03 09:35:23 +0900

    🎨 Update Notice Domian

--- server/src/main/java/com/ssafy/live/notice/controller/NoticeController.java
+++ server/src/main/java/com/ssafy/live/notice/controller/NoticeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/notices")
+@RequestMapping("/notices")
 public class NoticeController {
 
 }
