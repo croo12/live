b2cb372d5b8b28242a336b0c1b546de9b617d33e
신현광 <rhkd0363@gmail.com>
2023-02-06 02:03:07 +0900

    :art: Add Pagination Component and Style File
    Add Pagination Component and Style File

---
+++ server/src/components/common/Pagination.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import classes from "./Pagination.module.scss";
+
+const Pagination = ({ itemsPerPage, totalItems, paginate }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  useEffect(() => {
+    paginate(currentPage);
+  }, [currentPage]);
+
+  return (
+    <nav>
+      <ul className={classes.pagination}>
+        <p
+          className={classes.moveBtn}
+          onClick={
+            currentPage !== 1
+              ? () => {
+                  setCurrentPage(currentPage - 1);
+                }
+              : undefined
+          }
+        >
+          &lt;
+        </p>
+        {pageNumbers.map((number) => (
+          <li
+            key={number}
+            className={currentPage === number ? classes.active : undefined}
+            onClick={() => {
+              setCurrentPage(number);
+            }}
+          >
+            <p>{number}</p>
+          </li>
+        ))}
+        <p
+          className={classes.moveBtn}
+          onClick={
+            currentPage !== Math.ceil(totalItems / itemsPerPage)
+              ? () => {
+                  setCurrentPage(currentPage + 1);
+                }
+              : undefined
+          }
+        >
+          &gt;
+        </p>
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
