407d731c942d9404efe6f2adaae9fcd9d101ddee
BaekJeongEun <sophia2404@naver.com>
2023-02-07 22:32:32 +0900

    ⚡ Realtor With Items

--- server/src/main/java/com/ssafy/live/account/realtor/controller/dto/RealtorResponse.java
+++ server/src/main/java/com/ssafy/live/account/realtor/controller/dto/RealtorResponse.java
@@ -1,13 +1,15 @@
 package com.ssafy.live.account.realtor.controller.dto;
 
 import com.ssafy.live.account.realtor.domain.entity.Realtor;
-import com.ssafy.live.house.domain.entity.House;
-import com.ssafy.live.house.domain.entity.Item;
 import com.ssafy.live.review.domain.entity.Review;
-import lombok.*;
-
 import java.time.LocalDateTime;
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 public class RealtorResponse {
 
@@ -37,13 +39,13 @@ public class RealtorResponse {
 
         public static RealtorResponse.FindByRegion toEntity(Realtor r) {
             return RealtorResponse.FindByRegion.builder()
-                    .name(r.getName())
-                    .phone(r.getPhone())
-                    .corp(r.getCorp())
-                    .description(r.getDescription())
-                    .businessAddress(r.getBusinessAddress())
-                    .imageSrc(r.getImageSrc())
-                    .build();
+                .name(r.getName())
+                .phone(r.getPhone())
+                .corp(r.getCorp())
+                .description(r.getDescription())
+                .businessAddress(r.getBusinessAddress())
+                .imageSrc(r.getImageSrc())
+                .build();
         }
     }
 
@@ -66,17 +68,17 @@ public class RealtorResponse {
 
         public static FindDetail toEntity(Realtor realtor) {
             return RealtorResponse.FindDetail.builder()
-                    .no(realtor.getNo())
-                    .businessNumber(realtor.getBusinessNumber())
-                    .name(realtor.getName())
-                    .email(realtor.getEmail())
-                    .phone(realtor.getPhone())
-                    .corp(realtor.getCorp())
-                    .registrationNumber(realtor.getRegistrationNumber())
-                    .description(realtor.getDescription())
-                    .businessAddress(realtor.getBusinessAddress())
-                    .imageSrc(realtor.getImageSrc())
-                    .build();
+                .no(realtor.getNo())
+                .businessNumber(realtor.getBusinessNumber())
+                .name(realtor.getName())
+                .email(realtor.getEmail())
+                .phone(realtor.getPhone())
+                .corp(realtor.getCorp())
+                .registrationNumber(realtor.getRegistrationNumber())
+                .description(realtor.getDescription())
+                .businessAddress(realtor.getBusinessAddress())
+                .imageSrc(realtor.getImageSrc())
+                .build();
         }
     }
 
@@ -104,17 +106,17 @@ public class RealtorResponse {
             private String businessAddress;
             private static RealtorInfo toEntity(Realtor realtor) {
                 return RealtorInfo.builder()
-                        .no(realtor.getNo())
-                        .name(realtor.getName())
-                        .email(realtor.getEmail())
-                        .phone(realtor.getPhone())
-                        .imageSrc(realtor.getImageSrc())
-                        .businessNumber(realtor.getBusinessNumber())
-                        .businessAddress(realtor.getBusinessAddress())
-                        .corp(realtor.getCorp())
-                        .registrationNumber(realtor.getRegistrationNumber())
-                        .description(realtor.getDescription())
-                        .build();
+                    .no(realtor.getNo())
+                    .name(realtor.getName())
+                    .email(realtor.getEmail())
+                    .phone(realtor.getPhone())
+                    .imageSrc(realtor.getImageSrc())
+                    .businessNumber(realtor.getBusinessNumber())
+                    .businessAddress(realtor.getBusinessAddress())
+                    .corp(realtor.getCorp())
+                    .registrationNumber(realtor.getRegistrationNumber())
+                    .description(realtor.getDescription())
+                    .build();
             }
         }
         @Getter
@@ -127,16 +129,16 @@ public class RealtorResponse {
             private String address;
             private int floor;
             private String buildingName;
-            public static Items toEntity(Item item, String imageSrc, House house) {
+            public static Items toEntity(RealtorByRegionProjectionInterface item) {
                 return Items.builder()
-                        .itemNo(item.getNo())
-                        .imageSrc(imageSrc)
-                        .deposit(item.getDeposit())
-                        .monthlyRent(item.getRent())
-                        .address(house.getAddress())
-                        .floor(house.getFloor())
-                        .buildingName(item.getBuildingName())
-                        .build();
+                    .itemNo(item.getNo())
+                    .imageSrc(item.getImageSrc())
+                    .deposit(item.getDeposit())
+                    .monthlyRent(item.getRent())
+                    .address(item.getAddress())
+                    .floor(item.getFloor())
+                    .buildingName(item.getBuildingName())
+                    .build();
             }
         }
 
@@ -149,11 +151,11 @@ public class RealtorResponse {
             private LocalDateTime createDate;
             public static Reviews toEntity(Review review) {
                 return Reviews.builder()
-                        .reviewNo(review.getNo())
-                        .reviewInfo(review.getReviewInfo())
-                        .ratingScore(review.getRatingScore())
-                        .createDate(review.getCreatedDate())
-                        .build();
+                    .reviewNo(review.getNo())
+                    .reviewInfo(review.getReviewInfo())
+                    .ratingScore(review.getRatingScore())
+                    .createDate(review.getCreatedDate())
+                    .build();
             }
         }
 
