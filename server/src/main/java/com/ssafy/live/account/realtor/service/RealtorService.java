3c92225e05a65746165f691d0ab06c7f694288a0
tsFile <tsFile@github.com>
2023-02-08 15:22:21 +0900

    :ambulance: Wrong Word in Code
    Wrong Word in Code

--- server/src/main/java/com/ssafy/live/account/realtor/service/RealtorService.java
+++ server/src/main/java/com/ssafy/live/account/realtor/service/RealtorService.java
@@ -144,7 +144,7 @@ public class RealtorService {
     }
 
     public ResponseEntity<?> withdrawl(String token) {
-        if(jwtTokenProvider.validateToken(token)) {
+        if(!jwtTokenProvider.validateToken(token)) {
             return response.fail("잘못된 요청입니다.", HttpStatus.BAD_REQUEST);
         }
 
