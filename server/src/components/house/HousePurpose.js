d62f98064c0292b98777c4d1762b771cf5158dd4
신현광 <rhkd0363@gmail.com>
2023-02-08 16:33:24 +0900

    :lipstick: Add House Purpose Style File and Update Format
    Add House Purpose Style File and Update Format

--- server/src/components/house/HousePurpose.js
+++ server/src/components/house/HousePurpose.js
@@ -4,95 +4,74 @@
  *  - onClose(), 완료 후 모달 창 닫는 함수
  */
 import Logo from "../../assets/image/liveLogo.png";
+import classes from "./HousePurpose.module.scss";
 
 const HousePurpose = (props) => {
   return (
-    <>
-      <header style={{ margin: "20px" }}>
-        <img src={Logo} />
-        <button style={{ float: "right" }}>
-          <strong onClick={props.onClose}>X</strong>
+    <div className={classes.purposeContent}>
+      <header>
+        <img src={Logo} alt="Live Logo" />
+        <button type="button" onClick={props.onClose}>
+          ✖
         </button>
       </header>
-      <hr />
-      <div
-        style={{
-          height: "75vh",
-          padding: "20px 10px",
-          overflowY: "scroll",
-        }}
-      >
-        <div>
-          <h1>건물 종류</h1>
-          <br />
+      <div className={classes.purposeInner}>
+        <div className={classes.innerBlock}>
+          <h2>건물 종류</h2>
           <p>
-            건물 종류는 2020년 8월 21일 중개사법 개정에 의한 필수 표기
-            정보입니다.
+            <span>
+              건물 종류는 2020년 8월 21일 중개사법 개정에 의한 필수 표기
+              정보입니다.
+            </span>
           </p>
-        </div>
-        <br />
-        <div>
           <blockquote>
             중개대상물의 표시·광고 명시사항 세부기준 “중개대상물 종류”는
             「건축법」 제2조제1항제2호에 따른 건축물과 그 밖의 토지의 정착물로
             구분하여 표시하되, 건축물은 「건축법」 제2조제2항에 따른 건축물의
-            용도로 구분하여 표시하여야 한다. 다만, 미등기건물의 경우에는
-            “미등기건물”이라고 표시하여야 한다.
+            용도로 구분하여 표시하여야 한다. 다만,{" "}
+            <span>미등기건물의 경우에는 “미등기건물”이라고 표시</span>
+            하여야 한다.
           </blockquote>
         </div>
-        <br />
-        <div>
+
+        <div className={classes.innerBlock}>
           <h2>중개대상물 입력 방법</h2>
+          <blockquote>
+            그 외 중개대상물을 입력해주실 때는{" "}
+            <span>이모티콘을 사용하거나</span> 단독주택, 다중주택, 다가구주택,
+            공동주택, 아파트, 연립주택, 다세대주택, 기숙사, 제1종근린생활시설,
+            제2종근린생활시설 등{" "}
+            <span>정확한 건물 종류가 작성되지 않으면 반려</span> 될 수 있습니다.
+          </blockquote>
           <p>
-            그 외 중개대상물을 입력해주실 때는 이모티콘을 사용하거나 단독주택,
-            다중주택, 다가구주택, 공동주택, 아파트, 연립주택, 다세대주택,
-            기숙사, 제1종근린생활시설, 제2종근린생활시설 등 정확한 건물 종류가
-            작성되지 않으면 반려될 수 있습니다.
+            <span>
+              ☞ 건축법 시행령 [별표1]{" "}
+              <a
+                href="https://law.go.kr/%EB%B2%95%EB%A0%B9%EB%B3%84%ED%91%9C%EC%84%9C%EC%8B%9D/(%EA%B1%B4%EC%B6%95%EB%B2%95%EC%8B%9C%ED%96%89%EB%A0%B9,%EB%B3%84%ED%91%9C1)"
+                target="_blank"
+                rel="noreferrer"
+              >
+                용도별 건축물의 종류
+              </a>
+            </span>
           </p>
-          <br />
-          <p>
-            ☞ 건축법 시행령 [별표1]{" "}
-            <a
-              href="https://law.go.kr/%EB%B2%95%EB%A0%B9%EB%B3%84%ED%91%9C%EC%84%9C%EC%8B%9D/(%EA%B1%B4%EC%B6%95%EB%B2%95%EC%8B%9C%ED%96%89%EB%A0%B9,%EB%B3%84%ED%91%9C1)"
-              target="_blank"
-              rel="noreferrer"
-            >
-              용도별 건축물의 종류
-            </a>
-          </p>
-          <br />
-          <p>
-            "「건축법」 제2조제2항에 따른 건축물의 상위 용도"를 건축물대장에서
-            찾을 수 없을 경우 건축물대장에 쓰여 있는 층별용도 기준으로
-            작성해주세요.
-          </p>
+          <blockquote>
+            "「건축법」 제2조제2항에 따른 건축물의 상위 용도"를{" "}
+            <span>건축물대장에서 찾을 수 없을 경우 </span>
+            건축물대장에 쓰여 있는 <span>층별용도 기준으로 작성</span>해주세요.
+          </blockquote>
         </div>
-        <br />
-        <div>
-          <p>이렇게 작성해주세요!</p>
-          <figure
-            style={{
-              display: "flex",
-              padding: "20px",
-              margin: "10px",
-              borderRadius: "5px",
-              background: "rgb(235,236,237)",
-            }}
-          >
-            ⭕ 다가구주택
-          </figure>
-          이렇게 작성하면 안돼요!
-          <figure
-            style={{
-              display: "flex",
-              padding: "20px",
-              margin: "10px",
-              borderRadius: "5px",
-              background: "rgb(235,236,237)",
-            }}
-          >
+        <div className={classes.innerBlock}>
+          <p>
+            <span>이렇게 작성해주세요!</span>
+          </p>
+          <figure>⭕ 다가구주택</figure>
+          <p>
+            <span>이렇게 작성하면 안돼요!</span>
+          </p>
+          <figure>
             ❌
-            <div style={{ display: "flex", flexDirection: "column" }}>
+            <div>
               깨끗한 단독주택
               <mark>
                 <code>
@@ -101,18 +80,10 @@ const HousePurpose = (props) => {
               </mark>
             </div>
           </figure>
-          <figure
-            style={{
-              display: "flex",
-              padding: "20px",
-              margin: "10px",
-              borderRadius: "5px",
-              background: "rgb(235,236,237)",
-            }}
-          >
-            ❌ 💯
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              단독주택
+          <figure>
+            ❌
+            <div>
+              💯 단독주택
               <mark>
                 <code>
                   <small>→ 이모티콘이 들어가면 안돼요.</small>
@@ -121,34 +92,22 @@ const HousePurpose = (props) => {
             </div>
           </figure>
           <p>
-            단, 미등기건물 중 관계법령에 따라 건축허가나 신고 등을 하였으나
-            등기가 완료되지 않은 건축물인 경우에는 신청서에 기재된 용도를 같이
-            작성할 수 있습니다.
+            단, 미등기건물 중 관계법령에 따라{" "}
+            <span>
+              건축허가나 신고 등을 하였으나 등기가 완료되지 않은 건축물인 경우
+            </span>
+            에는 신청서에 기재된 용도를 같이 작성할 수 있습니다.
           </p>
-          <p>이렇게 작성해주세요!</p>
-          <figure
-            style={{
-              display: "flex",
-              padding: "20px",
-              margin: "10px",
-              borderRadius: "5px",
-              background: "rgb(235,236,237)",
-            }}
-          >
-            ⭕ 공동주택 (미등기 건물)
-          </figure>
-          <p>이렇게 작성하면 안돼요!</p>
-          <figure
-            style={{
-              display: "flex",
-              padding: "20px",
-              margin: "10px",
-              borderRadius: "5px",
-              background: "rgb(235,236,237)",
-            }}
-          >
+          <p>
+            <span>이렇게 작성해주세요!</span>
+          </p>
+          <figure>⭕ 공동주택 (미등기 건물)</figure>
+          <p>
+            <span>이렇게 작성하면 안돼요!</span>
+          </p>
+          <figure>
             ❌
-            <div style={{ display: "flex", flexDirection: "column" }}>
+            <div>
               공동주택 🚨 미등기 건물
               <mark>
                 <code>
@@ -158,12 +117,14 @@ const HousePurpose = (props) => {
             </div>
           </figure>
           <p>
-            단독주택, 다가구주택, 공동주택 등 정확한 건물 종류 작성을
-            부탁드립니다!
+            <span>
+              단독주택, 다가구주택, 공동주택 등 정확한 건물 종류 작성을
+              부탁드립니다!
+            </span>
           </p>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
