ed0f551fe90edb99f3f5994d30a2359ca53c05bd
신현광 <rhkd0363@gmail.com>
2023-02-10 15:51:25 +0900

    :art: Add persistStore
    Add persistStore

--- server/src/index.js
+++ server/src/index.js
@@ -6,11 +6,18 @@ import router from "./router/router";
 import { RouterProvider } from "react-router-dom";
 import store from "./store/store";
 import { Provider } from "react-redux";
+import persistStore from "redux-persist/es/persistStore";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const persistor = persistStore(store);
+
 root.render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <PersistGate loading={null} persistor={persistor}>
+      <RouterProvider router={router} />
+    </PersistGate>
   </Provider>
 );
 
