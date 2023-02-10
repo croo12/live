fcbeab967565de8a11313253b9463cf9b8ceb03c
김혜정 <mnb3559@gmail.com>
2023-02-10 16:41:38 +0900

    :bug: Change AllowedOrigins

--- server/src/main/java/com/ssafy/live/config/WebConfig.java
+++ server/src/main/java/com/ssafy/live/config/WebConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://live-live.store");
+        registry.addMapping("/**").allowedOrigins("*");
     }
 }
