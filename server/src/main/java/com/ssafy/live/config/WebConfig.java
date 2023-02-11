855d8fa71552713ab510fe7e0a3d35f8b9384be6
BaekJeongEun <sophia2404@naver.com>
2023-02-11 18:09:56 +0900

    🔒 Security Issue

--- server/src/main/java/com/ssafy/live/config/WebConfig.java
+++ server/src/main/java/com/ssafy/live/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://live-live.store", "https://localhost", "https://localhost:3000")
+                .allowedOrigins("https://live-live.store", "https://localhost", "https://localhost:3000", "http://localhost", "http://localhost:3000")
                 .allowedMethods("GET","POST","PUT","DELETE","PATHCH")
                 .allowCredentials(true);
     }
