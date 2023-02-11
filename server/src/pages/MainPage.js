e47b0274e05f867b457a87a182b3b5218622b16c
its19447@gmail.com <its19447@gmail.com>
2023-02-12 03:33:11 +0900

    :lipstick: Add Style Component

--- server/src/pages/MainPage.js
+++ server/src/pages/MainPage.js
@@ -3,10 +3,14 @@ import classes from "./MainPage.module.scss";
 import MainBanner from "../components/main/MainBanner";
 import MainContent from "../components/main/MainContent";
 import MainRanking from "../components/main/MainRanking";
+import CustomAlert from "../UI/Alert";
+import { useState } from "react";
 
 // 메인페이지
 
 const MainPage = () => {
+  const [isVisible, setVisible] = useState(false);
+
   return (
     <>
       <section>
@@ -18,6 +22,22 @@ const MainPage = () => {
       <section className={classes.ranking}>
         <MainRanking /> {/*지금 인기있는 공인중개사*/}
       </section>
+      <button
+        onClick={() => {
+          setVisible(true);
+        }}
+      >
+        알람을 만들어버리는 버튼
+      </button>
+      {isVisible && (
+        <CustomAlert
+          title={`나 강림`}
+          content={`내용? 그런건 딱히 없지만...`}
+          link={`/login`}
+          linkName={`로그인페이지로`}
+          setter={setVisible}
+        />
+      )}
     </>
   );
 };
