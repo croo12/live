93f1990b3e68211fdb709b7e9c20f6f093fed510
tsFile <tsFile@github.com>
2023-02-11 16:26:08 +0900

    :lock: Update Preflight Config
    Update Preflight Config

--- server/src/main/java/com/ssafy/live/config/WebConfig.java
+++ server/src/main/java/com/ssafy/live/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedHeaders("Authorization")
+                .allowedHeaders("*")
                 .allowCredentials(true).maxAge(3600);
     }
 }
