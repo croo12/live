d026b0507a33ab75101dc3e49488aa739a5cd59b
BaekJeongEun <sophia2404@naver.com>
2023-02-12 17:22:56 +0900

    ✨ Reservation List

--- server/src/apis/reservationApis.js
+++ server/src/apis/reservationApis.js
@@ -1,4 +1,4 @@
-import axiosInstance from "../util/axios";
+import axiosInstance, { getAuthHeader } from "../util/axios";
 
 export const searchRealtorList = (params) => {
   return axiosInstance.get(`realtors/region`, { params });
@@ -10,4 +10,16 @@ export const searchReservationRealtorDetail = (realtorNo, params) => {
 
 export const registResevation = (data) => {
   return axiosInstance.get(``);
+};
+
+export const getReservationList = async (data) => {
+  let result = null;
+  console.log("데이터 보내줘라")
+  await axiosInstance.get(`consultings?situation=${data}`, {
+      headers: getAuthHeader(),
+    }).then((data)=> {
+      result = data.data;
+      alert(result);
+    });
+    return result;
 };
