3ec056b03e9120999b9dae3caf6fd90659f9ac14
신현광 <rhkd0363@gmail.com>
2023-02-15 10:01:52 +0900

    👽️ Update HouseList, ContentList, Content

--- server/src/components/house/HouseList.js
+++ server/src/components/house/HouseList.js
@@ -1,145 +1,70 @@
-/**
- * 중개사 회원 등록 매물 목록
- */
-
+import HouseListContent from "./HouseListContent";
+import Pagination from "../common/Pagination";
+import classes from "./HouseList.module.scss";
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Button from "../../UI/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { getRealtorsHouseList } from "../../apis/houseApis";
 import { GiConversation } from "react-icons/gi";
 
-import classes from "./HouseList.module.scss";
-import HouseListContent from "./HouseListContent";
-
-import tempImg from "../../assets/image/sample.jpg";
-import Pagination from "../common/Pagination";
-import { getRealtorsHouseList } from "../../apis/houseApis";
-
-export const DUMMY_HOUSE_DATA = [
-  {
-    itemNo: "1",
-    image: tempImg,
-    price: "월세 1,000/501",
-    area: "23.14m²ㆍ반지하",
-    address: "용산구 청파동2가1",
-    description: "여유있는면적오픈형/지층이지만삭...",
-  },
-  {
-    itemNo: "2",
-    image: tempImg,
-    price: "월세 1,000/502",
-    area: "23.14m²ㆍ반지하",
-    address: "용산구 청파동2가2",
-    description: "여유있는면적오픈형/지층이지만삭...",
-  },
-  {
-    itemNo: "3",
-    image: tempImg,
-    price: "월세 1,000/503",
-    area: "23.14m²ㆍ반지하",
-    address: "용산구 청파동2가3",
-    description: "여유있는면적오픈형/지층이지만삭...",
-  },
-  {
-    itemNo: "4",
-    image: tempImg,
-    price: "월세 1,000/504",
-    area: "23.14m²ㆍ반지하",
-    address: "용산구 청파동2가4",
-    description: "여유있는면적오픈형/지층이지만삭...",
-  },
-  {
-    itemNo: "5",
-    image: tempImg,
-    price: "월세 1,000/505",
-    area: "23.14m²ㆍ반지하",
-    address: "용산구 청파동2가5",
-    description: "여유있는면적오픈형/지층이지만삭...",
-  },
-  {
-    itemNo: "6",
-    image: tempImg,
-    price: "월세 1,000/506",
-    area: "23.14m²ㆍ반지하",
-    address: "용산구 청파동2가6",
-    description: "여유있는면적오픈형/지층이지만삭...",
-  },
-  {
-    itemNo: "7",
-    image: tempImg,
-    price: "월세 1,000/507",
-    area: "23.14m²ㆍ반지하",
-    address: "용산구 청파동2가7",
-    description: "여유있는면적오픈형/지층이지만삭...",
-  },
-  {
-    itemNo: "8",
-    image: tempImg,
-    price: "월세 1,000/508",
-    area: "23.14m²ㆍ반지하",
-    address: "용산구 청파동2가8",
-    description: "여유있는면적오픈형/지층이지만삭...",
-  },
-  {
-    itemNo: "9",
-    image: tempImg,
-    price: "월세 1,000/509",
-    area: "23.14m²ㆍ반지하",
-    address: "용산구 청파동2가9",
-    description:
-      "여유있는면적오픈형/지층이지만삭가가가가가가가가가가가가가여유있는면적오픈형/지층이지만삭가가가가가가가가가가가가가여유있는면적오픈형/지층이지만삭가가가가가가가가가가가가가여유있는면적오픈형/지층이지만삭가가가가가가가가가가가가가",
-  },
-  {
-    itemNo: "10",
-    image: tempImg,
-    price: "월세 1,000/5010",
-    area: "23.14m²ㆍ반지하",
-    address: "용산구 청파동2가0",
-    description: "여유있는면적오픈형/지층이지만삭...",
-  },
-];
-
 const HouseList = () => {
-  const searchInputRef = useRef(); // 매물 검색 입력 Ref
-  const navigate = useNavigate(); // 페이지 이동을 위해 navigate 사용
+  const [houseList, setHouseList] = useState([]);
+  const [currList, setCurrList] = useState([]);
+  const searchInputRef = useRef();
+  const itemsPerPage = 5;
+  const totalList = useSelector((state) => {
+    return state.house.houseList;
+  });
 
-  const searchEventHandler = () => {
-    // 매물 검색 이벤트 함수
-    console.log(searchInputRef.current.value);
-
-    searchInputRef.current.value = "";
-  };
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    getRealtorsHouseList();
+    getRealtorsHouseList(dispatch);
   }, []);
 
-  const houseRegistHandler = () => {
-    // 매물 등록 페이지 이동 함수
-    navigate("/house/regist");
-  };
+  useEffect(() => {
+    setHouseList(totalList);
+  }, [totalList]);
 
-  const [houseList, setHouseList] = useState([]);
-
-  const paginateHandler = (pageNum) => {
-    let tempArr = [];
-    const itemsPerPage = 10;
-    let endNum = pageNum * itemsPerPage;
-    const srtNum = endNum - itemsPerPage;
-    endNum =
-      endNum > DUMMY_HOUSE_DATA.length ? DUMMY_HOUSE_DATA.length : endNum;
-
-    for (let i = srtNum; i < endNum; i++) {
-      tempArr.push(DUMMY_HOUSE_DATA[i]);
-    }
-
-    setHouseList(tempArr);
-  };
+  useEffect(() => {
+    setCurrList(houseList.slice(0, 5));
+  }, [houseList]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   });
 
-  //매물 페이지의 index
+  const searchEventHandler = () => {
+    const searchList = totalList.filter((item) => {
+      return (
+        item.address.includes(searchInputRef.current.value) ||
+        item.addressDetail.includes(searchInputRef.current.value)
+      );
+    });
+
+    setHouseList(searchList);
+
+    searchInputRef.current.value = "";
+  };
+
+  const paginateHandler = (pageNum) => {
+    let tempArr = [];
+    let endNum = pageNum * itemsPerPage;
+    const srtNum = endNum - itemsPerPage;
+    endNum = endNum > houseList.length ? houseList.length : endNum;
+
+    for (let i = srtNum; i < endNum; i++) {
+      tempArr.push(houseList[i]);
+    }
+
+    setCurrList(tempArr);
+  };
+
+  const houseRegistHandler = () => {
+    navigate("/house/regist");
+  };
+
   return (
     <div className={classes.houseList}>
       <div className={classes.listInner}>
@@ -161,24 +86,30 @@ const HouseList = () => {
           </div>
         </div>
         <div className={classes.registContent}>
-          <Button clickEvent={houseRegistHandler}>매물 등록</Button>
+          <button onClick={houseRegistHandler}>매물 등록</button>
         </div>
-        <div>
-          <HouseListContent houses={houseList} />
-        </div>
-        <Pagination
-          itemsPerPage={3}
-          totalItems={DUMMY_HOUSE_DATA.length}
-          paginate={paginateHandler}
-        />
-        <div className={classes.inputButton}>
-          <input
-            type="text"
-            ref={searchInputRef}
-            placeholder="지역명을 입력해주세요"
-          />
-          <Button clickEvent={searchEventHandler}>검 색</Button>
-        </div>
+        {!houseList ? (
+          <p> 등록된 매물이 없습니다</p>
+        ) : (
+          <>
+            <div>
+              <HouseListContent houses={currList} />
+            </div>
+            <Pagination
+              itemsPerPage={itemsPerPage}
+              totalItems={houseList.length}
+              paginate={paginateHandler}
+            />
+            <div className={classes.inputButton}>
+              <input
+                type="text"
+                ref={searchInputRef}
+                placeholder="지역명을 입력해주세요"
+              />
+              <button onClick={searchEventHandler}>검 색</button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
--- server/src/components/house/HouseListContent.js
+++ server/src/components/house/HouseListContent.js
@@ -4,25 +4,43 @@ import classes from "./HouseListContent.module.scss";
 
 const ListItem = (props) => {
   const house = props.house;
-  const responsiveTextLen = [10, 15, 21, 25, 30];
 
+  const responsiveTextLen = [10, 15, 30, 35, 42];
   return (
     <div className={classes.houseItem}>
       <div className={classes.houseImg}>
         <img src={house.image} alt={house.address} />
       </div>
       <div className={classes.houseInfo}>
-        <h3>{house.price}</h3>
+        <h3>
+          월세 {house.deposit.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+          만원/ {house.rent.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+          만원
+        </h3>
         <div>
-          <p>{house.area}</p>
+          <p>
+            전용{" "}
+            {house.exclusivePrivateArea
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            m² (
+            {Math.round(house.exclusivePrivateArea / 3.3)
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            평) ㆍ 층수
+          </p>
           <ResponsiveText
-            text={house.address}
+            text={house.address + " " + house.addressDetail}
             textLength={responsiveTextLen}
             className={classes.desc}
           />
         </div>
         <ResponsiveText
-          text={house.description}
+          text={
+            house.description
+              ? house.description
+              : "매물에 작성된 설명이 존재하지 않습니다."
+          }
           textLength={responsiveTextLen}
           className={classes.desc}
         />
@@ -35,18 +53,28 @@ const HouseListContent = (props) => {
   const houses = props.houses;
   const navigate = useNavigate();
 
+  console.log(houses);
+
   const navigateDetailHandler = (itemNo) => {
     navigate(`/house/detail/${itemNo}`);
   };
   return (
     <ul className={classes.houseList}>
-      {houses.map((house) => (
-        <div className={classes.itemContent} key={house.itemNo}>
-          <li onClick={() => navigateDetailHandler(house.itemNo)}>
-            <ListItem house={house} />
+      {houses.length !== 0 ? (
+        houses.map((house) => (
+          <div className={classes.itemContent} key={house.itemNo}>
+            <li onClick={() => navigateDetailHandler(house.itemNo)}>
+              <ListItem house={house} />
+            </li>
+          </div>
+        ))
+      ) : (
+        <div className={classes.itemContent}>
+          <li>
+            <strong>등록된 매물이 존재하지 않습니다.</strong>
           </li>
         </div>
-      ))}
+      )}
     </ul>
   );
 };
