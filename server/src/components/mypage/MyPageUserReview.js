0aef2eb5af41789b5e7b2cb4190e5454ccee9eb2
신현광 <rhkd0363@gmail.com>
2023-02-17 03:48:54 +0900

    💄 ⚡️ Chage Style

--- server/src/components/mypage/MyPageUserReview.js
+++ server/src/components/mypage/MyPageUserReview.js
@@ -2,38 +2,33 @@ import { useEffect, useState } from "react";
 import classes from "./MyPageUserReview.module.scss";
 import ListBox from "../../UI/ListBox";
 import ReviewCardContent from "../ReviewCardContent";
-import {getReviewList} from "../../apis/reviewApis"
+import { getReviewList } from "../../apis/reviewApis";
 
 const MyPageUserReview = () => {
-
   const [reviewList, setReviewList] = useState();
 
   useEffect(() => {
     try {
-      getReviewList()
-      .then(response => {setReviewList(response.data.data)});
+      getReviewList().then((response) => {
+        setReviewList(response.data.data);
+      });
     } catch (error) {
       console.log(error);
     }
   }, []);
 
-  if(!reviewList){
+  if (!reviewList) {
     return null;
   }
 
   return (
     <>
-      일반회원 리뷰 조회
       <div className={classes.review}>
         <div className={classes.viewReview}>
           <div className={classes.inner}>
             <h3>리뷰조회</h3>
             <div className={classes.reviewContent}>
-              <ListBox
-                style={{ "justify-content": "space-between" }}
-                dataArray={reviewList}
-                direction={true}
-              >
+              <ListBox dataArray={reviewList} direction={true}>
                 <ReviewCardContent />
               </ListBox>
             </div>
