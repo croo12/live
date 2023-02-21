28a3694da909369925be359e3bbf0a308dd2de98
tsFile <tsFile@github.com>
2023-02-22 00:35:10 +0900

    :art: Update MyPageRealtor CSS
    Update MyPageRealtor CSS

--- server/src/components/mypage/MyPageRealtor.js
+++ server/src/components/mypage/MyPageRealtor.js
@@ -37,23 +37,22 @@ const MyPageRealtor = () => {
           <div className={classes.introContent}>
             <div className={classes.info}>
               <div className={classes.leftImg}>
-                <img
-                  alt="프로필"
-                  src={userInfo.profile !== null ? userInfo.profile : sample}
-                ></img>
+                <img alt="프로필" src={userInfo.profile !== null ? userInfo.profile : sample}></img>
               </div>
               <div className={classes.rightDesc}>
-                <p>
-                  안녕하세요, <strong>{userInfo.name}</strong>님<br />
-                </p>
-                <div className={classes.infoState}>
-                  <span>중개사 회원</span>
-                  <button onClick={onClickHandler}>개인정보 조회</button>
+                <div className={classes.mainProfileContainer}>
+                  <div className={classes.mainProfileContent}>
+                    안녕하세요, <strong>{userInfo.name}</strong>님
+                  </div>
+                  <p>중개사 회원</p>
                 </div>
-                <div className={classes.temperatureInfo}>
-                  <div className={classes.temperature}>
-                    <img alt="★" src={star} className={classes.star}></img>
-                    <div className={classes.starNum}>{userInfo.score}</div>
+                <div className={classes.infoState}>
+                  <button onClick={onClickHandler}>개인정보 조회</button>
+                  <div className={classes.temperatureInfo}>
+                    <div className={classes.temperature}>
+                      <img alt="★" src={star} className={classes.star}></img>
+                      <div className={classes.starNum}>{userInfo.score}</div>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -64,11 +63,7 @@ const MyPageRealtor = () => {
       <div className={classes.itemBox}>
         <div className={classes.inner}>
           <div className={classes.itemBoxContent}>
-            <div
-              className={`${classes.alarm} ${
-                btnActive === 1 ? classes.active : ""
-              }`}
-            >
+            <div className={`${classes.alarm} ${btnActive === 1 ? classes.active : ""}`}>
               <button onClick={reviewonClickHandler}>
                 <div className={classes.leftLogo}>
                   <BsFillBellFill />
@@ -78,11 +73,7 @@ const MyPageRealtor = () => {
                 </div>
               </button>
             </div>
-            <div
-              className={`${classes.review} ${
-                btnActive === 2 ? classes.active : ""
-              }`}
-            >
+            <div className={`${classes.review} ${btnActive === 2 ? classes.active : ""}`}>
               <button onClick={contractOnClickHandler}>
                 <div className={classes.leftLogo}>
                   <BsSearch />
@@ -92,11 +83,7 @@ const MyPageRealtor = () => {
                 </div>
               </button>
             </div>
-            <div
-              className={`${classes.reservation} ${
-                btnActive === 3 ? classes.active : ""
-              }`}
-            >
+            <div className={`${classes.reservation} ${btnActive === 3 ? classes.active : ""}`}>
               <button onClick={reservationonClickHandler}>
                 <div className={classes.leftLogo}>
                   <TfiWrite />
